3564e3f7fc84bcc49c062966cbd1448a4deb6677
abdullahyazji36 <abdullahyazji36@gmail.com>
2026-06-29 09:52:38 +0300

    Header, NavBar

---
+++ src/component/header/Header.tsx
@@ -0,0 +1,15 @@
+"use client";
+import NavBar from "./NavBar";
+
+
+
+const Header = () => {
+
+    return (
+        <header className="absolute inset-x-0 top-0 z-50">
+            <NavBar />
+        </header>
+    )
+}
+
+export default Header
---
+++ src/component/header/NavBar.tsx
@@ -0,0 +1,97 @@
+import { useState } from "react";
+import MenuIcon from '@mui/icons-material/Menu';
+import MenuOpen from "@mui/icons-material/MenuOpen";
+import { Button, Dialog, DialogContent, Container } from "@mui/material";
+
+const navigation = [
+    { name: 'Home', href: '#home' },
+    { name: 'Projects', href: '#projects' },
+    { name: 'Skills', href: '#' },
+    { name: 'About', href: '#' },
+]
+
+const NavBar = () => {
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+    return (
+        <>
+
+            <nav aria-label="Global" className="fixed top-0 left-0 w-full z-50">
+                <Container>
+                    <div className="flex items-center justify-between p-6 lg:px-8">
+                        <div className="flex lg:flex-1">
+                            <a href="#" className="-m-1.5 p-1.5">
+                                <span className="text-3xl">Abdullah</span>
+                            </a>
+                        </div>
+                        <div className="flex lg:hidden">
+                            <button
+                                type="button"
+                                onClick={() => setMobileMenuOpen(true)}
+                                className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 dark:text-gray-200"
+                            >
+                                <span className="sr-only">Open main menu</span>
+                                <MenuIcon aria-hidden="true" className="size-6" />
+                            </button>
+                        </div>
+                        <div className="hidden lg:flex lg:gap-x-12">
+                            {navigation.map((item) => (
+                                <a key={item.name} href={item.href} className="text-sm/6 font-semibold hover:text-blue-600 transition">
+                                    {item.name}
+                                </a>
+                            ))}
+                        </div>
+                        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+                            <Button component="a" href="#" variant="contained">
+                                Contact me <span aria-hidden="true">&rarr;</span>
+                            </Button>
+                        </div>
+                    </div>
+                </Container>
+            </nav>
+
+            <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
+                <div className="fixed inset-0 z-50" />
+                <DialogContent className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-100/10">
+                    <div className="flex items-center justify-between">
+                        <a href="#" className="-m-1.5 p-1.5">
+                            <span className="text-3xl text-white">Abdullah</span>
+                        </a>
+                        <button
+                            type="button"
+                            onClick={() => setMobileMenuOpen(false)}
+                            className="-m-2.5 rounded-md p-2.5 text-gray-200"
+                        >
+                            <span className="sr-only">Close menu</span>
+                            <MenuOpen aria-hidden="true" className="size-6" />
+                        </button>
+                    </div>
+                    <div className="mt-6 flow-root">
+                        <div className="-my-6 divide-y divide-white/10">
+                            <div className="space-y-2 py-6">
+                                {navigation.map((item) => (
+                                    <a
+                                        key={item.name}
+                                        href={item.href}
+                                        className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-white/5"
+                                    >
+                                        {item.name}
+                                    </a>
+                                ))}
+                            </div>
+                            <div className="py-6">
+                                <a
+                                    href="#"
+                                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-white hover:bg-white/5"
+                                >
+                                    Contact me
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </DialogContent>
+            </Dialog>
+        </>
+    )
+}
+
+export default NavBar
