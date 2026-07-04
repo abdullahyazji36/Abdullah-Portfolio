5bfa83f6ebc3a4e5f4152ca8eeee9360fcfa0968
abdullahyazji36 <abdullahyazji36@gmail.com>
2026-07-04 10:33:12 +0300

    Create Footer

---
+++ src/component/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="flex items-center justify-center text-slate-400 pt-12 pb-5 text-xs">
+            <p>Copyright © 2026 </p>
+        </div>
+    )
+}
+
+export default Footer
