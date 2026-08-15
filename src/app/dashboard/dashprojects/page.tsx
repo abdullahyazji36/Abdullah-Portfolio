301b39d7f1bea7c658d0ba1ac7afee8467a32015
abdullahyazji36 <abdullahyazji36@gmail.com>
2026-08-15 20:11:36 +0300

    Dashboard Projects Page

---
+++ src/app/dashboard/dashprojects/page.tsx
@@ -0,0 +1,59 @@
+import prisma from "@/lib/prisma";
+import Link from "next/link";
+import { auth } from "@/auth";
+
+
+const page = async () => {
+    const session = await auth();
+
+    const projects = await prisma.project.findMany({
+        where: {
+            userId: session?.user?.id
+        }
+    });
+    return (
+        <section className="mt-8">
+            <div className="overflow-hidden rounded-xl border border-gray-200 bg-white shadow-sm dark:border-gray-700 dark:bg-gray-800">
+                <div className="overflow-x-auto">
+                    <table className="w-full text-left text-sm text-gray-600 dark:text-gray-300">
+                        <thead className="bg-gray-100 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-300">
+                            <tr>
+                                <th scope="col" className="px-6 py-4">
+                                    Project
+                                </th>
+                                <th scope="col" className="px-6 py-4 text-right">
+                                    Action
+                                </th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            {projects.map((project) => (
+                                <tr
+                                    key={project.id}
+                                    className="border-b border-gray-200 bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600"
+                                >
+                                    <td className="px-6 py-4 font-medium text-gray-900 dark:text-white">
+                                        {project.title}
+                                    </td>
+
+                                    <td className="px-6 py-4 text-right">
+                                        <Link
+                                            href={`/dashboard/dashprojects/${project.id}`}
+                                            className="inline-block rounded-lg bg-blue-600 px-4 py-2 text-sm font-medium text-white transition hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                        >
+                                            View Project
+                                        </Link>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </section>
+
+    )
+}
+
+export default page
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,14 @@
-import { projects } from '@/utils/mockData';
+// import { projects } from '@/utils/mockData';
 import { skills } from '@/utils/mockData';
 import { signOutUser } from '@/actions/auth.action';
 import { Button } from '@mui/material';
+import Link from 'next/link';
+import prisma from "@/lib/prisma";
 
-const Dashboard = () => {
+
+const Dashboard = async () => {
+
+    const projectCount = await prisma.project.count()
 
     return (
         <>
@@ -12,17 +17,21 @@ const Dashboard = () => {
             </h1>
 
             <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 xl:grid-cols-3">
-                <div className="rounded-xl p-6 shadow">
+                <div className="rounded-xl p-6 shadow bg-gray-50 dark:bg-gray-800">
                     <h2 className="text-lg font-semibold">
                         Projects
                     </h2>
 
                     <p className="mt-4 text-3xl font-bold">
-                        {projects.length}
+                        {projectCount}
                     </p>
+                    <Link href="/dashboard/dashprojects"
+                        className="inline-block mt-4 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+                    >
+                        All Projects</Link>
                 </div>
 
-                <div className="rounded-xl  p-6 shadow">
+                <div className="rounded-xl  p-6 shadow bg-gray-50 dark:bg-gray-800">
                     <h2 className="text-lg font-semibold">
                         Skills
                     </h2>
